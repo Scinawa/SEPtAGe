9185db66df982fd6cd7490487fd1f4ee107031e3
Scinawa <alessandro.luongo@studenti.unimi.it>
2016-01-26 15:27:06 +0100

    Unify API interface.

--- stable.py
+++ stable.py
@@ -12,7 +12,21 @@ def iterable_partitions(number=0):
     return A_PARTITION
 
 
+################# Networkx ###################################
+
+
 def stable_partitions_from_networkx(G):
+    """
+    TODO: check if directed or undirected.
+    :param G: a NetworkX graph.
+    :return: an iterator over the list of stable partitions
+    """
+    import networkx as nx
+    partitions = iterable_partitions(len(G.nodes()))
+    edge_list = EdgeList(G, kind='netwokrx')
+    return find_stable_partitions(partitions, edge_list)
+
+def all_stable_partitions_from_networkx(G):
     """
     TODO: check if directed or undirected.
     :param G: a NetworkX graph.
@@ -24,15 +38,32 @@ def stable_partitions_from_networkx(G):
 
     return
 
+################# NUMPY ###################################
 
 def stable_partition_from_numpy(matrix):
     """
-    To be defined
-    :param matrix:
+    It calculates the stable partition of a numpy adjacency matrix.
+
+    :param matrix: numpy matrix
     :return:
     """
-    edge_list = EdgeList()
-    return find_all_stable_partitions(EdgeList(matrix, kind=""))
+    edge_list = EdgeList(matrix, kind='numpy')
+    partitions = iterable_partitions(edge_list.node_number)
+    return find_stable_partitions(partitions, edge_list)
+
+def all_stable_partition_from_numpy(matrix):
+    """
+    It calculates the stable partition of a numpy adjacency matrix.
+
+    :param matrix: numpy matrix
+    :return:
+    """
+    edge_list = EdgeList(matrix, kind='numpy')
+    partitions = iterable_partitions(edge_list.node_number)
+    return find_all_stable_partitions(partitions, edge_list)
+
+
+################# GENERIC ###################################
 
 
 def find_all_stable_partitions(partitions, edge_list):
@@ -48,7 +79,7 @@ def find_all_stable_partitions(partitions, edge_list):
     return stable_partitions
 
 
-def find_stable_partitions(edge_list, partitions):
+def find_stable_partitions(partitions, edge_list):
     """
     Given an (iterable) list of partitions and the graph, find the next
     stable partition. This is the old version of the algorithm, that iterates
@@ -71,13 +102,13 @@ def find_stable_partitions(edge_list, partitions):
     for a_part in partitions:
         unstable = 0
         for edge in edge_list:
-            print('edge1', i)
+            print('Edge', edge)
             jump = 0
             for bucket in [i for i in a_part if len(i) > 1]:  # (1)
-                print("\t\til bucket", bucket)
+                print("\t\tthe bucket", bucket)
                 for node in bucket:
-                    if (node == edge[0] or node == edge[1]):
-                        if (edge[0] == edge[1]): # (3)
+                    if node == edge[0] or node == edge[1]:
+                        if edge[0] == edge[1]:  # (3)
                                 unstable_number += 1
                                 unstable = 1
                                 break  # good job!
@@ -95,49 +126,3 @@ def find_stable_partitions(edge_list, partitions):
             if jump or unstable: break
         if not unstable:
             yield a_part
-
-def is_stable(partition, edges):
-    for i in edges:
-        edge = grafo.edgeList[i]
-        for bucket in [i for i in a_part if len(i) > 1]:  # skip bucket <= 1
-            a = str(edge[0])
-            b = str(edge[1])
-            j = 0
-            for chr in bucket:
-                j = j + 1
-                chr = str(chr)
-                # se ho trovato un arco, devo solo cercare l'altro
-                # perche'  sono sicuro che tutta la partizione sia stabile
-                if (
-                        chr == a or chr == b):  # print "trovato un carattere", a,b,chr
-                    # print "\t\t\t\ttrovato in partizione", a_part, "bucket", bucket, "chr", chr, "a,", a, "b, ", b
-                    other2find = set(set((a, b,)) - set((chr,)))
-                    chr2 = int(other2find.pop())
-                    skip1 = 1
-                    for succ in range(j, len(bucket)):
-                        # print "\t\t\t\tbucket[succ]",bucket[succ]
-                        if (chr2 == bucket[
-                            succ]):  # print "trovato il secondo"
-                            unstableNumber = unstableNumber + 1
-                            # print "\t\t\tnon stabile:", a_part,a,b,bucket,unstableNumber
-                            skip2 = 1
-                            break  # smette di cercare nei caratteri del bucket
-                    break  # smette di cercare carattere nel bucket,
-                    # tanto so che ha trovato un nodo, l'altro non e' dentro
-                    #  e quindi sara' da un'altra parte, e quindi e' stabile (skip=0)
-                else:
-                    # print "nessuno dei due char in bucket", bucket, a,b
-                    # pass # non ho trovato uno dei due caratteri, vado al succesivo
-                    pass
-                    # if (skip1): # se ho trovato un solo carattere, skippo i bucket e vado all'arco successivo.
-                    #    break
-        if (skip2):  # non serve controllare per ogni arco che questa
-            # non sia stabile se non e gia stabile
-            # print "break arco"
-            break
-        if (not skip2):
-            # print "stabile: ", a_part
-            stabili.append(a_part)
-            yield a_part
-            skip2 = 0
-    return stabili
