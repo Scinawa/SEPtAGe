8015e912a66637abf8edf9c1d0e7c11b0af3380f
Scinawa <alessandro.luongo@studenti.unimi.it>
2016-01-28 02:14:18 +0100

    edited algo after testing

--- stable.py
+++ stable.py
@@ -3,7 +3,7 @@ __author__ = 'scinawa'
 from graph import EdgeList
 from partitionsets import ordered_set
 from partitionsets import partition
-
+import datetime
 
 def iterable_partitions(number=0):
     A_LIST = list(range(0, number))
@@ -65,7 +65,17 @@ def all_stable_partition_from_numpy(matrix):
 
 ################# GENERIC ###################################
 
+def timeMeasure(function_to_decorate):
+	def a_wrapper_accepting_arbitrary_arguments(*args, **kwargs):
 
+		d1=datetime.datetime.now()
+		result=function_to_decorate(*args, **kwargs)
+		d2=datetime.datetime.now()
+
+		return (d2-d1, result)
+	return a_wrapper_accepting_arbitrary_arguments
+
+@timeMeasure
 def find_all_stable_partitions(partitions, edge_list):
     """
     Iterate over all yelds and give all the stable partition of the graph.
@@ -74,12 +84,12 @@ def find_all_stable_partitions(partitions, edge_list):
     :return: a list of stable partitions
     """
     stable_partitions = []
-    for i in find_stable_partitions(partitions, edge_list):
+    for i in find_stable_partition(partitions, edge_list):
         stable_partitions.append(i)
     return stable_partitions
 
 
-def find_stable_partitions(partitions, edge_list):
+def find_stable_partition(partitions, edge_list):
     """
     Given an (iterable) list of partitions and the graph, find the next
     stable partition. This is the old version of the algorithm, that iterates
@@ -88,30 +98,30 @@ def find_stable_partitions(partitions, edge_list):
     for performances reasons.
 
     There are some "hacks" in order to improve the performance of the algorithm:
-    (1) If the bucket of the partition is empty or it has only one element =>
-    skip the bucket and go to the next one
-    (2) If you find JUST one side of the edge in one bucket, it means that for
+    (1) If you find JUST one side of the edge in one bucket, it means that for
     that edge, the partition i stable and you can skip to check the next edge
-    (3) If you are testing for an auto loop =>
 
     :param partitions: the iterable of partitions by partitionset package
     :param edge_list: the list of edges [(a,b), (c,b), ... ]
     :return: yield an iterable of stable partitions
     """
+
     unstable_number = 0
     for a_part in partitions:
+        print ("\nPartition: ", a_part, end='')
         unstable = 0
         for edge in edge_list:
-            print('Edge', edge)
+            print(' Edge', edge, end='')
             jump = 0
-            for bucket in [i for i in a_part if len(i) > 1]:  # (1)
-                print("\t\tthe bucket", bucket)
+            for bucket in [i for i in a_part]:
+                print(" Bucket", a_part.index(bucket), end='')
                 for node in bucket:
                     if node == edge[0] or node == edge[1]:
-                        if edge[0] == edge[1]:  # (3)
+                        if node == edge[0] and node == edge[1]:  # autoloop
                                 unstable_number += 1
                                 unstable = 1
-                                break  # good job!
+                                print (" Unstable (autoloop)")
+                                break
                         else:
                             other2find = {edge[0], edge[1]} - {node}
                             missing_node = other2find.pop()
@@ -119,10 +129,11 @@ def find_stable_partitions(partitions, edge_list):
                                 if missing_node == other_nodes:
                                     unstable_number += 1
                                     unstable = 1
+                                    print (" Unstable (connection)")
                                     break  # good job!
-                        jump = 1  # one extreme found, goto next edge (2)
-                        break
-                if jump or unstable: break
-            if jump or unstable: break
+                    else:  # (1)
+                        jump = 1
+                if jump or unstable: break # (1)
         if not unstable:
             yield a_part
+
